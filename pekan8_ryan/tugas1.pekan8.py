b12125b9317be93c698978bec3a49e6257524427
ryanraditya <146079241+ryanraditya@users.noreply.github.com>
2023-11-22 11:51:42 +0700

    Add files via upload

---
+++ pekan8_ryan/tugas1.pekan8.py
@@ -0,0 +1,6 @@
+def profil(nama,kelas,alamat,hobby):
+    print("Nama Saya:",nama)
+    print("Rombel Saya:",kelas)
+    print("Alamat Lengkap saya:",alamat)
+    print("Hobby Saya adalah:",hobby)
+profil("Ryan Raditya Prayoga","Ti03","Jln Abdul Ghani rt01 rw04 no41","Badminton")
